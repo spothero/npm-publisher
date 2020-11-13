149f423f5f3f98d2bf949fe92ee4d5a7193ef99b
Boiar Qin <boiar@spothero.com>
2020-11-13 15:44:29 -0600

    test: Default to master

--- bin/npm-release.js
+++ bin/npm-release.js
@@ -2,6 +2,6 @@
 const ReleaseOps = require('../lib/release');
 
 const prereleaseId = (process.env.NPM_PRERELEASE) ? process.env.NPM_PRERELEASE : null;
-const branchName = (process.env.BRANCH_NAME) ? process.env.BRANCH : null;
+const branchName = (process.env.BRANCH_NAME) ? process.env.BRANCH : 'master';
 
 ReleaseOps.release(prereleaseId, branchName);
