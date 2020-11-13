9aba3036a4b7d7788ff05134872294a43ed443d0
Boiar Qin <boiar@spothero.com>
2020-11-13 15:01:49 -0600

    test: Allow passing of branch name as a param

--- bin/npm-release.js
+++ bin/npm-release.js
@@ -2,5 +2,6 @@
 const ReleaseOps = require('../lib/release');
 
 const prereleaseId = (process.env.NPM_PRERELEASE) ? process.env.NPM_PRERELEASE : null;
+const branchName = (process.env.BRANCH_NAME) ? process.env.BRANCH : null;
 
-ReleaseOps.release(prereleaseId);
+ReleaseOps.release(prereleaseId, branchName);
